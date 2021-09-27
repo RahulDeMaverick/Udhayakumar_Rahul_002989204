d170ce216ccbdc22fc829725c9bd4a69975a597b
RahulDeMaverick <udhayakumar.r@northeastern.edu>
2021-09-26 23:12:20 -0400

    initial commit

---
+++ Lab_1/src/model/PersonalData.java
@@ -0,0 +1,170 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+/**
+ *
+ * @author rahuludhayakumar
+ */
+public class PersonalData {
+    
+    private String name;
+    private String geographicData;
+    private String dateOfBirth;
+    private long mobileNumber;
+    private long faxNumber;
+    private String emailAddress;
+    private String ssn;
+    private int medicalRecordNumber;
+    private int healthPlanNumber;
+    private long bankAccountNumber;
+    private String vehicleIdentifier;
+    private long serialNumber;
+    private String linkedIn;
+    private String ipAddress;
+    private String biometricID;
+    private String photo;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getGeographicData() {
+        return geographicData;
+    }
+
+    public void setGeographicData(String geographicData) {
+        this.geographicData = geographicData;
+    }
+
+    public String getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(String dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public long getMobileNumber() {
+        return mobileNumber;
+    }
+
+    public void setMobileNumber(long mobileNumber) {
+        this.mobileNumber = mobileNumber;
+    }
+
+    public long getFaxNumber() {
+        return faxNumber;
+    }
+
+    public void setFaxNumber(long faxNumber) {
+        this.faxNumber = faxNumber;
+    }
+
+    public String getEmailAddress() {
+        return emailAddress;
+    }
+
+    public void setEmailAddress(String emailAddress) {
+        this.emailAddress = emailAddress;
+    }
+
+    public String getSsn() {
+        return ssn;
+    }
+
+    public void setSsn(String ssn) {
+        this.ssn = ssn;
+    }
+
+    public int getMedicalRecordNumber() {
+        return medicalRecordNumber;
+    }
+
+    public void setMedicalRecordNumber(int medicalRecordNumber) {
+        this.medicalRecordNumber = medicalRecordNumber;
+    }
+
+    public int getHealthPlanNumber() {
+        return healthPlanNumber;
+    }
+
+    public void setHealthPlanNumber(int healthPlanNumber) {
+        this.healthPlanNumber = healthPlanNumber;
+    }
+
+    public long getBankAccountNumber() {
+        return bankAccountNumber;
+    }
+
+    public void setBankAccountNumber(long bankAccountNumber) {
+        this.bankAccountNumber = bankAccountNumber;
+    }
+
+    public String getVehicleIdentifier() {
+        return vehicleIdentifier;
+    }
+
+    public void setVehicleIdentifier(String vehicleIdentifier) {
+        this.vehicleIdentifier = vehicleIdentifier;
+    }
+
+    public long getSerialNumber() {
+        return serialNumber;
+    }
+
+    public void setSerialNumber(long serialNumber) {
+        this.serialNumber = serialNumber;
+    }
+
+    public String getLinkedIn() {
+        return linkedIn;
+    }
+
+    public void setLinkedIn(String linkedIn) {
+        this.linkedIn = linkedIn;
+    }
+
+    public String getIpAddress() {
+        return ipAddress;
+    }
+
+    public void setIpAddress(String ipAddress) {
+        this.ipAddress = ipAddress;
+    }
+
+    public String getBiometricID() {
+        return biometricID;
+    }
+
+    public void setBiometricID(String biometricID) {
+        this.biometricID = biometricID;
+    }
+
+    public String getPhoto() {
+        return photo;
+    }
+
+    public void setPhoto(String photo) {
+        this.photo = photo;
+    }
+
+    public String getIdCode() {
+        return idCode;
+    }
+
+    public void setIdCode(String idCode) {
+        this.idCode = idCode;
+    }
+    private String idCode;
+    
+   
+    
+}
