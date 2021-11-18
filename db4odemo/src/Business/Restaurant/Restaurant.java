1114028f8bfb8f0d0dfdd9fc3e91f96ba5918ea7
RahulDeMaverick <udhayakumar.r@northeastern.edu>
2021-11-18 12:55:28 -0500

    first commit

---
+++ db4odemo/src/Business/Restaurant/Restaurant.java
@@ -0,0 +1,61 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Business.Restaurant;
+
+/**
+ *
+ * @author rahuludhayakumar
+ */
+public class Restaurant {
+
+   
+    
+    private String resNo;
+    private String resName;
+    private String resAddress;
+    private String resManager;
+    private String resContact;
+    
+     public String getResNo() {
+        return resNo;
+    }
+
+    public void setResNo(String resNo) {
+        this.resNo = resNo;
+    }
+
+    public String getResName() {
+        return resName;
+    }
+
+    public void setResName(String resName) {
+        this.resName = resName;
+    }
+
+    public String getResAddress() {
+        return resAddress;
+    }
+
+    public void setResAddress(String resAddress) {
+        this.resAddress = resAddress;
+    }
+
+    public String getResManager() {
+        return resManager;
+    }
+
+    public void setResManager(String resManager) {
+        this.resManager = resManager;
+    }
+
+    public String getResContact() {
+        return resContact;
+    }
+
+    public void setResContact(String resContact) {
+        this.resContact = resContact;
+    }
+}
